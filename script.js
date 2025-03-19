a767306bf09bb10fb25522caabc35fe881cac513
lucassantanadias <luksd382@gmail.com>
2025-03-19 17:15:58 -0300

    commit

---
+++ script.js
@@ -0,0 +1,1 @@
+setTimeout(function(){document.getElementById("cta-button").style.display="inline-block";document.getElementById("cta-button").classList.add("show")},5000); setTimeout(function(){document.getElementById("feature").style.display="flex";document.getElementById("feature").classList.add("show")},7000);
